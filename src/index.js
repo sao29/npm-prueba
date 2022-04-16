04edb8bfc8f9c755d15de52a917e498fc99674f3
Salvador Alvarez <sao.95@hotmail.com>
2022-04-15 23:26:11 -0700

    proyecto prueb subido en npm

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const messages = [
+    "salvador",
+    "jose",
+    "pedro",
+    "juan",
+    "diego"
+];
+
+
+const randomMsg = () =>{
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+
+}
+
+module.exports = { randomMsg};
